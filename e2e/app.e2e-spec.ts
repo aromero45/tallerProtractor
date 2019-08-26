77b19b322646f9579d5ca76d4b2c40cf682e8ed0
aromero45 <viejo4543@gmail.com>
2019-08-25 23:50:23 -0500

    Desarrollo de scripts para protractor

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -33,3 +33,33 @@ describe('Tour of heroes, heroes page', () => {
   });
 
 });
+
+describe('Tour of heroes, search a hero', () => {
+  let page: TourOfHeroesPage;
+
+  beforeEach(() => {
+    page = new TourOfHeroesPage;
+    page.navigateTo();
+  });
+
+  it('should search a hero', () => {
+    expect(page.searchAHeroe('Mr. Nice')).toBe('Mr. Nice');
+  });
+
+});
+
+describe('Tour of heroes, elinate a hero', () => {
+  let page: TourOfHeroesPage;
+
+  beforeEach(() => {
+    page = new TourOfHeroesPage;
+    page.navigateToHeroes();
+  });
+
+  it('should eliminate a hero', () => {
+    const numHeroes = page.getAllHeroes().count();
+    page.eliminateAHeroe('18');
+    expect(page.getAllHeroes().count()).toBe(numHeroes.then(n => n - 1));
+  });
+
+});
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -21,4 +21,24 @@ export class TourOfHeroesPage {
     element(by.tagName('input')).sendKeys(newHero);
     element(by.buttonText('Add')).click();
   }
+
+  searchAHeroe(name: string){
+    element(by.id('search-box')).sendKeys(name);
+    return element.all(by.css('.search-result')).filter((res)=>{
+      return res.getText().then(function(value){
+        console.log("Mostrar valores filtrados: ", value);
+        return value === name;
+      })
+    }).first().getText().then(function(text){
+      console.log("Heroe buscado: ", text);
+      return text;
+    });
+  }
+  eliminateAHeroe(id: string){
+    element.all(by.css('.heroes li')).filter(function(list) {
+      return list.element(by.css('.badge')).getText().then(function(val) {
+        return val === id;
+      });
+    }).first().element(by.css('.delete')).click();
+  }
 }
